4878ae024fc7089338ddfa3de8e846dc3029f919
imanajjari <iman.najjari9494@gmail.com>
2025-08-03 00:04:14 +0330

    Update PositionedText.jsx

--- src/components/Models/PositionedText.jsx
+++ src/components/Models/PositionedText.jsx
@@ -8,7 +8,7 @@ export default function PositionedText({ position, text, onClick = null, color =
     <Html position={position} center zIndexRange={[0, 0]}>
       
       <div
-  className="rounded-lg text-3xl xl:text-sm px-2 py-1 whitespace-nowrap transition-opacity duration-100 transform -translate-x-[-100px] xl:translate-x-1/2 -translate-y-1/2 "
+  className="rounded-lg text-3xl xl:text-sm px-2 py-1 whitespace-nowrap transition-opacity duration-100 transform -translate-x-[-100px] xl:translate-x-1/2 -translate-y-1/2 cursor-pointer"
   style={{
     background,
     color,
