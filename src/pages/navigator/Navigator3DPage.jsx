6cf6871fd9b76c035bbd0a2c4ab718e7f1f0a34a
imanajjari <iman.najjari9494@gmail.com>
2025-09-21 12:33:37 +0330

    added useCheckStandAndCompany to Navigator3DPage for detect login

--- src/pages/navigator/Navigator3DPage.jsx
+++ src/pages/navigator/Navigator3DPage.jsx
@@ -13,6 +13,7 @@ import usePathPoints from "../../hooks/navigator/usePathPoints";
 import Navigator3DScene from "./Navigator3DScene";
 import PageLoader from "../../components/scene/PageLoader";
 import NavigatorSearchResultsModal from "../../components/Modal/NavigatorSearchResultsModal";
+import useCheckStandAndCompany from "../../hooks/useCheckStandAndCompany"
 import NavigatorShopDetailsModal from "../../components/Modal/NavigatorShopDetailsModal";
 
 const BASE_OFFSET = 0;
@@ -21,6 +22,9 @@ export default function Navigator3DPage() {
   const { floors, hasFloors, getFloorByName } = useFloors();
   const { path, updateCurrentFloorNumber, refreshLastDestination, lastDestination } = usePath();
   const { colors } = useTheme();
+
+  // برای وقتی که اطلاعات استند ها و طبقات رو نداشته باشه ؛ می ره برای لاگین
+  useCheckStandAndCompany();
 
   const { isPortrait, verticalOffset } = useOrientation(BASE_OFFSET);
   const { activeFloor, currentModelFile, floorDestinations, handleFloorSelect } =
